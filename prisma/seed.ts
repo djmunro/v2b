7081da375fe9e54d2f169f35f6c157d9d9da7e0d
David Munro <munrodj86@gmail.com>
2021-04-04 07:48:40 -0400

    all the things

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import faker from "faker";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = [...new Array(5)].map(() => {
+    return {
+      email: faker.internet.email(),
+      name: faker.name.findName(),
+      image: faker.image.image(),
+      builds: {
+        create: {
+          title: faker.lorem.words(),
+          link: faker.internet.url(),
+          notes: faker.lorem.sentence(),
+          heroClass: faker.lorem.word(),
+          heroCareer: faker.lorem.words(),
+        },
+      },
+    };
+  });
+
+  for (let data of users) {
+    const user = await prisma.user.create({ data });
+    console.log(user);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => prisma.$disconnect);
