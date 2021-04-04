b02a21a60377e3e409493b9ecae5be65edfdd1f2
David Munro <munrodj86@gmail.com>
2021-04-04 07:57:05 -0400

    Added chakaraui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider session={pageProps.session}>
-      <Component {...pageProps}></Component>
+      <ChakraProvider>
+        <Component {...pageProps}></Component>
+      </ChakraProvider>
     </Provider>
   );
 };
