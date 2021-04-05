baa0e4515a208f3ba55ccbb038fc8fcebb9d1ab3
David Munro <munrodj86@gmail.com>
2021-04-05 07:24:22 -0400

    Added Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+
+import queryClient from "lib/react-query";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider session={pageProps.session}>
-      <ChakraProvider>
-        <Component {...pageProps}></Component>
-      </ChakraProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Provider session={pageProps.session}>
+          <ChakraProvider>
+            <Component {...pageProps}></Component>
+          </ChakraProvider>
+        </Provider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 };
 
